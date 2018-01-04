aa9ad989ff4a9c85dde636754cdd4f03f907af99
Regulareveryday <julia.sypien@gmail.com>
2018-01-04 14:06:52 +0000

    adding objects on detected faces

--- Main.py
+++ Main.py
@@ -1,29 +1,3 @@
-'''# Load two images
-img1 = cv2.imread('messi5.jpg')
-img2 = cv2.imread('opencv_logo.png')
-
-# I want to put logo on top-left corner, So I create a ROI
-rows,cols,channels = img2.shape
-roi = img1[0:rows, 0:cols ]
-
-# Now create a mask of logo and create its inverse mask also
-img2gray = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
-ret, mask = cv2.threshold(img2gray, 10, 255, cv2.THRESH_BINARY)
-mask_inv = cv2.bitwise_not(mask)
-
-# Now black-out the area of logo in ROI
-img1_bg = cv2.bitwise_and(roi,roi,mask = mask_inv)
-
-# Take only region of logo from logo image.
-img2_fg = cv2.bitwise_and(img2,img2,mask = mask)
-
-# Put logo in ROI and modify the main image
-dst = cv2.add(img1_bg,img2_fg)
-img1[0:rows, 0:cols ] = dst
-
-cv2.imshow('res',img1)
-cv2.waitKey(0)
-cv2.destroyAllWindows()'''
 import numpy as np
 import cv2
 #from matplotlib import pyplot as plt
@@ -83,7 +57,7 @@ if __name__ == '__main__':
     
         # Draw a rectangle around the faces
         for (x, y, w, h) in faces:
-            '''img2 = cv2.imread('mustache-and-glasses.jpg')
+            img2 = cv2.imread('mustache-and-glasses.jpg')
             res = cv2.resize(img2,(w, h), interpolation = cv2.INTER_CUBIC)
             #cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
             rows, cols, channels = res.shape
@@ -96,12 +70,12 @@ if __name__ == '__main__':
             img2_fg = cv2.bitwise_and(res,res,mask = mask_inv)
             # Put logo in ROI and modify the main image
             dst = cv2.add(img1_bg,img2_fg)
-            frame[y:(y+h), x:(x+w)] = dst'''
-            img2 = cv2.imread('hat.jpg')
+            frame[y:(y+h), x:(x+w)] = dst
+            '''img2 = cv2.imread('hat.jpg')
             res = cv2.resize(img2,(w, h), interpolation = cv2.INTER_CUBIC)
             #cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
             rows, cols, channels = res.shape
-            roi = frame[(y-h)):y, x:(x+w)]
+            roi = frame[(y-h):y, x:(x+w)]
             
             img2gray = cv2.cvtColor(res,cv2.COLOR_BGR2GRAY)
             ret, mask = cv2.threshold(img2gray, 10, 255, cv2.THRESH_BINARY)
@@ -117,33 +91,7 @@ if __name__ == '__main__':
             img2_fg = cv2.bitwise_and(res,res,mask = mask_inv)
             # Put logo in ROI and modify the main image
             dst = cv2.add(img1_bg,img2_fg)
-            frame[(y-h):y, x:(x+w)] = dst
-
-
-
-            
-            '''# I want to put logo on top-left corner, So I create a ROI
-rows,cols,channels = img2.shape
-roi = img1[0:rows, 0:cols ]
-
-# Now create a mask of logo and create its inverse mask also
-img2gray = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
-ret, mask = cv2.threshold(img2gray, 10, 255, cv2.THRESH_BINARY)
-mask_inv = cv2.bitwise_not(mask)
-
-# Now black-out the area of logo in ROI
-img1_bg = cv2.bitwise_and(roi,roi,mask = mask_inv)
-
-# Take only region of logo from logo image.
-img2_fg = cv2.bitwise_and(img2,img2,mask = mask)
-
-# Put logo in ROI and modify the main image
-dst = cv2.add(img1_bg,img2_fg)
-img1[0:rows, 0:cols ] = dst
-
-cv2.imshow('res',img1)
-cv2.waitKey(0)
-cv2.destroyAllWindows()'''
+            frame[(y-h):y, x:(x+w)] = dst'''
         
 
         colormap_num = cv2.getTrackbarPos('colormap', 'image')
